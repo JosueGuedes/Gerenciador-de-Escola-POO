f67813bacfe0c4c88533c54f267ba894378f0243
JosueGuedes <139816376+JosueGuedes@users.noreply.github.com>
2024-04-17 23:14:36 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 public class Turma {
     String codigo_turma, materia;
-    static int base = 100;
+    static int base = 1000;
     Pessoa discente;
-    HashMap<String, Pessoa> lista_de_alunos = new HashMap<String, Pessoa>(); // A string em questão é codigo do aluno
-    HashMap<Pessoa, Float> notas = new HashMap<Pessoa, Float>();
+    public HashMap<String, Pessoa> lista_de_alunos = new HashMap<String, Pessoa>(); 
+    public HashMap<String, Float> notas = new HashMap<String, Float>(); // A string em questão é codigo do aluno em ambos os casos
 
     public Turma(String materia){
-        this.codigo_turma = "T" + Integer.toString(base);
-        base += 10;
+        this.codigo_turma = Integer.toString(base);
+        base += 1;
         this.materia = materia;
         Scanner scan = new Scanner(System.in);
         System.out.println("Nome do discente: ");
-        // String nome = scan.nextLine();
-        this.discente = new Pessoa(scan.nextLine(), "Professor");
-        scan.close();
+        String nome = scan.nextLine();
+        this.discente = new Pessoa(nome, "Professor");
+        //scan.close();
     }
 
     public void adicionar_aluno(){
@@ -27,15 +27,25 @@ public class Turma {
         Pessoa aluno_adicionado = new Pessoa(nome, "Aluno"); // Um aluno so precisa ter seu nome informado, visto que seu codigo é feito automaticamente
         // E sua ocupação é implicita
         lista_de_alunos.put(aluno_adicionado.codigo, aluno_adicionado);
-
-        scan.close();
     }
 
-    public void excluir_aluno(){
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Codigo do aluno: ");
-        String codigo_do_excluido = scan.nextLine();
-        lista_de_alunos.remove(codigo_do_excluido);
-        scan.close();
+    public void get_data(){
+        System.out.println("Cod. turma: " + codigo_turma + "\nProfessor: " + discente.nome + "\nMatéria: " + materia);
+        System.out.println("Lista de alunos: ");
+
+        for (Pessoa estudantes : lista_de_alunos.values()){ // Vai buscar na lista de alunos (que é da classe Pessoa) 
+            System.out.println("\t"+estudantes.nome+"\tCod: "+estudantes.codigo);
+        }
+        System.out.println();
+    }
+
+    public void atribuir_nota(Pessoa aluno, Float nota){
+        notas.put(aluno.getCode(), nota);
+    }
+
+    public void mostrar_notas(){
+        for (Pessoa estudantes : lista_de_alunos.values()){
+            System.out.println("Aluno: "+ estudantes.nome + "\tNota: " + notas.get(estudantes.codigo));
+        }
     }
 }
