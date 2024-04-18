78216bea913682c4dd926b7069d159c6d9191aa2
JosueGuedes <139816376+JosueGuedes@users.noreply.github.com>
2024-04-17 23:13:52 -0300

    Update Escola.java
    Postos os comandos carregar e salvar (passíveis de mudança), os comandos de criação de turma, professor e aluno.

--- Escola.java
+++ Escola.java
@@ -1,11 +1,18 @@
 package Person;
 import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 
 public class Escola {
     String nome_escola;
     HashMap<String, Turma> turmas_registradas = new HashMap<String, Turma>();
-    HashMap<String, Pessoa> alunos_registrados = new HashMap<String, Pessoa>(); 
+    HashMap<String, Pessoa> alunos_registrados = new HashMap<String, Pessoa>();
+    public HashMap<String, Pessoa> professores_registrados = new HashMap<String, Pessoa>();
     // A string nos casos acima são o código da turma e do aluno, respectivamente
     
     public void criar_turma(){
@@ -19,7 +26,53 @@ public class Escola {
         System.out.println("Matéria: " + turma_criada.materia);
     }
 
-    public void criar_aluno(){
-
+    public void criar_aluno(String nome, String materia){
+        Pessoa aluno_add = new Pessoa(nome, "Aluno");
+        alunos_registrados.put(aluno_add.codigo, aluno_add);
     }
+
+    public void criar_professor(String nome){
+        Pessoa discente_registrado = new Pessoa(nome, "Professor");
+        professores_registrados.put(discente_registrado.codigo, discente_registrado);
+    }
+
+    public void salvar(Escola school){
+        try {
+            FileOutputStream fos = new FileOutputStream("src/Person/dados.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(school); oos.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        } catch (IOException r){
+            System.out.println("Erro encontrado: " + r);
+        }
+    }
+
+    public Escola carregar(){
+        Escola escola_carregada = new Escola();
+        try {
+            FileInputStream fos = new FileInputStream("src/Person/dados.txt");
+            ObjectInputStream oos = new ObjectInputStream(fos);
+            escola_carregada = (Escola) oos.readObject();
+            oos.close();
+            return escola_carregada;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        } catch (IOException r){
+            System.out.println("Erro encontrado: " + r);
+        } catch (ClassNotFoundException s){
+            System.out.println("to bluelockado");
+        }
+        return escola_carregada;
+    }
+
+    public int check_numProfessores(){
+        return this.professores_registrados.size();
+    }
+
+    public int check_numTurmas(){
+        return this.turmas_registradas.size();
+    }
+
 }
